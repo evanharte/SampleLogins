bf1e90e347676dde113717fe9f9625b8e48742a0
Evan Harte <126525806+evanharte@users.noreply.github.com>
2024-03-07 11:52:42 -0330

    basic site working

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ const PORT = 3000;
 
 global.DEBUG = true;
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { name: "Evan" });
+  res.render("index.ejs", { name: "EvzaursRex" });
 });
 
-app.get("/404", (req, res) => {
-  res.render("404");
+app.use((req, res) => {
+  res.status(404).render("404");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Simple app running on port ${PORT}`);
 });
