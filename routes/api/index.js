0393314706d6b7fb248004a2dfa628a00d8d18cd
Evan <evan.harte@keyin.com>
2024-03-12 21:56:02 -0230

    35 mins into class commit

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,13 +2,18 @@ var router = require("express").Router();
 
 if (DEBUG) {
   // console.log("ROUTE: /api/actors");
-  console.log("ROUTE: /api/logins");
+  console.log("ROUTE: /api");
   // console.log("ROUTE: /api/full");
   // console.log("ROUTE: /api/users");
 }
 // http://localhost:3000/api/actors/
 // const actorsRouter = require("./actors");
 // router.use("/actors", actorsRouter);
+
+// http://localhost:3000/api/
+router.get("/", (req, res) => {
+  res.send("API Home Page");
+});
 
 // http://localhost:3000/api/logins/
 const loginsRouter = require("./logins");
