cd1753e6df395385c09165f654029d89096c5a84
Evan <evan.harte@keyin.com>
2024-03-19 20:04:56 -0230

    Add dotenv configuration (FOR SECURITY) / updated database connection settings

--- services/pg.auth_db.js
+++ services/pg.auth_db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "evanharte",
-  host: "localhost",
-  database: "Auth",
-  password: "12345",
-  port: 5432,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 });
 
 if (DEBUG) console.log("connected to PostgreSQL...");
